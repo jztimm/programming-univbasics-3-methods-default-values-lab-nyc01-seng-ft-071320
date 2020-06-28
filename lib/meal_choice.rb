e30cbfc7c8d62116b43d0577c9ce69134c1bbe44
Jzavier Timm <jztimm@gmail.com>
2020-06-28 23:25:19 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 
 # Make a method called meal_choice with 3 parameters
 
-def meal_choice(veg1, veg2, protein = tofu)
+def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
